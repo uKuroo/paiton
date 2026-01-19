a0dda3447294a0f45505ae925b0d257bea85299f
uKuroo <opitimalracer50@hotmail.com>
2026-01-19 01:15:09 -0300

    feat(): done

---
+++ Config.py
@@ -0,0 +1,18 @@
+OBSERVED_FOLDER = "/home/gsantos/Downloads/paiton/"
+BASE_EXTENSION_FOLDER = "/home/gsantos/Arquivos/"
+
+PREFIX_FOLDERS = {
+    "INF": "Informatica/",
+    "PRJ": "Projeto/"
+}
+
+EXTENSIONS_FOLDERS = {
+    ".jpg": "IMAGEM/",
+    ".png": "IMAGEM/",
+    ".gif": "GIF/",
+    ".mp4": "VIDEO/",
+    ".mkv": "VIDEO/",
+    ".wmv": "VIDEO/",
+    ".mp3": "AUDIO/",
+    ".wav": "AUDIO/"
+}
--- Main.py
+++ Main.py
@@ -1,1 +1,30 @@
-print("Hello World")
+import Config
+from pathlib import Path
+
+pasta = Path(Config.OBSERVED_FOLDER)
+
+if(pasta.exists()):
+    arquivos = [arquivo for arquivo in pasta.iterdir() if arquivo.is_file()]
+
+    for arquivo in arquivos:
+        nomeArquivo = arquivo.name
+        pastaDestino = Config.BASE_EXTENSION_FOLDER
+        
+        prefix = arquivo.name.split("_")[0]
+
+        if(Config.PREFIX_FOLDERS.get(prefix) != None):
+            pastaDestino += Config.PREFIX_FOLDERS[prefix]
+            nomeArquivo = arquivo.name[len(prefix)+1:]
+        else:
+            pastaDestino += "Outros/"
+
+        pastaDestino += Config.EXTENSIONS_FOLDERS[arquivo.suffix]
+        
+        Path(pastaDestino).mkdir(parents=True, exist_ok=True)
+
+        pastaDestino += nomeArquivo
+
+        pastaFinal = Path(pastaDestino)
+        print(pastaFinal)
+        arquivo.move(pastaFinal)
+
